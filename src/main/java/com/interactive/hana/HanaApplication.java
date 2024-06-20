b550771b7aad91dba981fa80be8a168b08c6a447
friendshipkim97 <rlawjddn5980@naver.com>
2024-06-20 16:00:17 +0900

    init commit

---
+++ src/main/java/com/interactive/hana/HanaApplication.java
@@ -0,0 +1,14 @@
+package com.interactive.hana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class HanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanaApplication.class, args);
+    }
+
+}
