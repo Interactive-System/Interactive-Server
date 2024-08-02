636a66912338347f930935685d909b629b09956d
friendshipkim97 <rlawjddn5980@naver.com>
2024-08-02 23:44:07 +0900

    시큐리티 설정 변경

--- src/main/java/com/interactive/hana/global/config/security/SecurityConfig.java
+++ src/main/java/com/interactive/hana/global/config/security/SecurityConfig.java
@@ -40,6 +40,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/v1/**/dashboard/**").permitAll() // dashboard가 포함된 URL에 대한 접근 허용
                 .antMatchers("/signup").permitAll()
                 .antMatchers("/api/v1/email-auth").permitAll()
+                .antMatchers("/image/**").permitAll()
+                .antMatchers("/file-save").permitAll()
+                .antMatchers("/api/v1/email-auth").permitAll()
                 .antMatchers("/api/v1/user/**")
                 .hasAnyRole("USER", "UW", "INSURANCE_PLANNER","INSURANCE_SELLER", "INSURANCE_COMPENSATION_PLANNER",
                         "INSURANCE_COMPENSATION_HANDLER", "CONTRACT_MANAGER", "FINANCIAL_SUPERVISORY_SERVICE", "ADMIN")
--- src/main/java/com/interactive/hana/global/file/api/FileApi.java
+++ src/main/java/com/interactive/hana/global/file/api/FileApi.java
@@ -29,7 +29,7 @@ public class FileApi {
         try (InputStream imageStream = new FileInputStream(System.getProperty("user.dir") + "/images/" + name)) {
             return ResponseEntity.ok(IOUtils.toByteArray(imageStream));
         } catch (IOException e) {
-            throw new IllegalArgumentException("해당 파일을 찾을 수 업습니다.");
+            throw new IllegalArgumentException("해당 파일을 찾을 수 없습니다.");
         }
     }
 
