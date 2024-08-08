c11d8f1761ab3f762a9554ced699060e34956efd
friendshipkim97 <rlawjddn5980@naver.com>
2024-08-08 17:36:33 +0900

    key 수정

--- src/main/java/com/interactive/hana/global/config/security/jwt/JwtProperties.java
+++ src/main/java/com/interactive/hana/global/config/security/jwt/JwtProperties.java
@@ -1,7 +1,7 @@
 package com.interactive.hana.global.config.security.jwt;
 
 public interface JwtProperties {
-    String SECRET_KEY = "DOGFOOTAWESOME";
+    String SECRET_KEY = "INTERACTIVEHANA";
     int EXPIRATION_TIME = 1000 * 60 * 60;
     String TOKEN_PREFIX = "Bearer ";
     String HEADER_STRING = "Authorization";
